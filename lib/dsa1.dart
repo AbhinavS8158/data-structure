62c02ad261392cf4b11618a548721c507614d799
AbhinavS8158 <abhinav.8158s@gmail.com>
2024-09-12 14:35:49 +0530

    update lib

--- lib/dsa1.dart
+++ lib/dsa1.dart
@@ -1,3 +1,43 @@
-int calculate() {
-  return 6 * 7;
+class Node{
+  int data;
+  Node?next;
+  Node(this.data);
+}
+class Lindedlist{
+  Node?head;
+
+
+  void insert(int data){
+    final newnode =Node(data);
+    if(head==null){
+      head=newnode;
+
+    }else{
+      Node?current=head;
+      while(current?.next!=null){
+        current=current?.next;
+      }
+      current?.next=newnode;
+    }
+  }
+  void prepend(int data){
+    Node?newnode=Node(data);
+    newnode.next=head;
+    head=newnode;
+  }
+  void display(){
+    Node?current=head;
+    while(current!=null){
+      print(current.data);
+       current=current?.next;
+  }
+    }
+   
+}
+void main(){
+  final list =Lindedlist();
+  list.insert(7);
+  list.prepend(2);
+  list.insert(9);
+  list.display();
 }
