607c88681c0b728498e5cfe1194b588d5115f6bd
AbhinavS8158 <abhinav.8158s@gmail.com>
2024-09-12 14:53:28 +0530

    updated code

--- lib/dsa1.dart
+++ lib/dsa1.dart
@@ -1,43 +1,78 @@
-class Node{
+class Node {
   int data;
-  Node?next;
+  Node? next;
+  
   Node(this.data);
 }
-class Lindedlist{
-  Node?head;
 
+class LinkedList {
+  Node? head;
 
-  void insert(int data){
-    final newnode =Node(data);
-    if(head==null){
-      head=newnode;
-
-    }else{
-      Node?current=head;
-      while(current?.next!=null){
-        current=current?.next;
+  // Insert at the end of the list
+  void insert(int data) {
+    final newNode = Node(data);
+    
+    if (head == null) {
+      head = newNode;
+    } else {
+      Node? current = head;
+      while (current?.next != null) {
+        current = current?.next;
       }
-      current?.next=newnode;
+      current?.next = newNode;
     }
   }
-  void prepend(int data){
-    Node?newnode=Node(data);
-    newnode.next=head;
-    head=newnode;
+
+  // Insert at the beginning of the list
+  void prepend(int data) {
+    Node newNode = Node(data);
+    newNode.next = head;
+    head = newNode;
   }
-  void display(){
-    Node?current=head;
-    while(current!=null){
+
+  // Insert at a specific position in the list
+  void add(int data, int position) {
+    Node newNode = Node(data);
+    
+    if (position == 0) {
+      newNode.next = head;
+      head = newNode;
+      return;
+    }
+
+    Node? current = head;
+    int index = 0;
+    
+    while (current != null && index < position - 1) {
+      current = current.next;
+      index++;
+    }
+    
+    if (current == null) {
+      print("Position out of bounds.");
+      return;
+    }
+    
+    newNode.next = current.next;
+    current.next = newNode;
+  }
+
+  // Display all nodes in the list
+  void display() {
+    Node? current = head;
+    
+    while (current != null) {
       print(current.data);
-       current=current?.next;
-  }
+      current = current.next;
     }
-   
+  }
 }
-void main(){
-  final list =Lindedlist();
+
+void main() {
+  final list = LinkedList();
   list.insert(7);
   list.prepend(2);
   list.insert(9);
+  list.add(34, 2);
   list.display();
 }
