96ffb4f9457b303ec9ccfc17af3604a0858c4db7
AbhinavS8158 <abhinav.8158s@gmail.com>
2024-09-17 15:22:45 +0530

    Fibnocci series

---
+++ lib/dsa3.dart
@@ -0,0 +1,12 @@
+Fibnocci(int num){
+  if(num==0){
+    return 0;
+  }else if(num==1){
+    return 1;
+  }else{
+    return Fibnocci(num-1)+Fibnocci(num-2);
+  }
+}
+void main (){
+  print (Fibnocci(6));
+}
