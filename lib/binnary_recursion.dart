813ae28f3f6ae4d308a05142679a3b54cbfb84ab
AbhinavS8158 <abhinav.8158s@gmail.com>
2024-09-17 16:09:05 +0530

    binnary search using recursion

---
+++ lib/binnary_recursion.dart
@@ -0,0 +1,25 @@
+Binarysearch(List<int>numbers, int start,int end,int target){
+if(start>end){
+  return -1;
+}
+int mid= start+end ~/2;
+if(numbers[mid]==target){
+  return  mid;
+}else if(numbers[mid]>target){
+  return Binarysearch(numbers,start,mid-1,target);
+}
+else{
+  return Binarysearch(numbers,mid+1,end,target);
+}
+}
+void main(){
+  List<int>arr=[1,2,3,4,5,6];
+  int target=10;
+  int result=Binarysearch(arr, 0, arr.length-1, target);
+   if(result!=-1){
+    print("target found at index:$result");
+   }else{
+    print("target not founded");
+   }
+
+}
