33154bba99a74fc22b24489408cf705dac39005a
AbhinavS8158 <abhinav.8158s@gmail.com>
2024-09-19 09:02:54 +0530

    occarace

--- lib/string.dart
+++ lib/string.dart
@@ -1,7 +1,25 @@
-String reverseEachWord(String input) {
-  return input.split(' ').map((word) => word.split('').reversed.join()).join(' ');
+// String reverseEachWord(String input) {
+//   return input.split(' ').map((word) => word.split('').reversed.join()).join(' ');
+// }
+
+// void main() {
+//   print(reverseEachWord("Hello World"));  
+// }
+
+Map<String, int> countCharacters(String input) {
+  Map<String, int> charCount = {};
+  for (var char in input.split('')) {
+    if (charCount.containsKey(char)) {
+      charCount[char] = charCount[char]! + 1;
+    } else {
+      charCount[char] = 1;
+    }
+  }
+  return charCount;
 }
 
+
 void main() {
-  print(reverseEachWord("Hello World"));  
+  print(countCharacters("Hello World"));
+
 }
